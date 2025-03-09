d1083cc7e651e36c71160dcbe5782882f777ab4c
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-03-08 22:19:36 -0300

    feat: complete contact development

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,15 +1,38 @@
 function Contact() {
     return (
-        <section id="contact" className="h-screen flex flex-col justify-center items-center">
-            <h2 className="text-3xl">Contact</h2>
-            <ul className="mt-4 flex gap-4">
-                <li>LinkedIn</li>
-                <li>GitHub</li>
-                <li>WhatsApp</li>
-                <li>Email</li>
-            </ul>
+        <section className="flex flex-col justify-center items-center gap-10 m-20 text-center text-color-purple">
+            <p>If you find my work interesting and want to get in touch, you can reach me through...</p>
+
+            <div className="flex justify-center items-center gap-10">
+                <a
+                    href="https://www.linkedin.com/in/federicogildemuro/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-color-yellow hover:underline"
+                >
+                    LinkedIn
+                </a>
+
+                <a
+                    href="https://wa.me/5492234248732"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-color-yellow hover:underline"
+                >
+                    WhatsApp
+                </a>
+
+                <a
+                    href="mailto:federicogildemuro@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-color-yellow hover:underline"
+                >
+                    E-mail
+                </a>
+            </div>
         </section>
     );
 }
 
-export default Contact;  
+export default Contact;
