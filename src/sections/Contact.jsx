7cb5c4ecf5e30f5a5e12dcbb8c17ec2a5044056d
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-03-10 11:54:36 -0300

    refactor(contact): update styles and add aria-labels

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,14 +1,15 @@
 function Contact() {
     return (
-        <section className="flex flex-col justify-center items-center gap-10 m-20 text-center text-color-purple">
+        <section className="flex flex-col justify-start items-center gap-10 p-10 lg:p-20 text-center text-color-pink">
             <p>If you find my work interesting and want to get in touch, you can reach me through...</p>
 
-            <div className="flex justify-center items-center gap-10">
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-10">
                 <a
                     href="https://www.linkedin.com/in/federicogildemuro/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-color-yellow hover:underline"
+                    className="text-color-white hover:underline"
+                    aria-label="Contact me on LinkedIn"
                 >
                     LinkedIn
                 </a>
@@ -17,7 +18,8 @@ function Contact() {
                     href="https://wa.me/5492234248732"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-color-yellow hover:underline"
+                    className="text-color-white hover:underline"
+                    aria-label="Send me a message on WhatsApp"
                 >
                     WhatsApp
                 </a>
@@ -26,7 +28,8 @@ function Contact() {
                     href="mailto:federicogildemuro@gmail.com"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-color-yellow hover:underline"
+                    className="text-color-white hover:underline"
+                    aria-label="Send me an email"
                 >
                     E-mail
                 </a>
