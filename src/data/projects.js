3f10bf1eb0ccb5ea7e3f03059b11c083199dbce5
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-03-19 09:33:34 -0300

    chore: update projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,15 +2,25 @@ const projects = [
     {
         name: 'My Portfolio',
         description: 'Personal portfolio website showcasing my skills, projects, and contact information.',
-        techIcons: ['react-plain colored', 'tailwindcss-plain colored'],
+        techIcons: [
+            { name: 'React', class: 'devicon-react-original' },
+            { name: 'Tailwind', class: 'devicon-tailwindcss-original' },
+        ],
         github: 'https://github.com/federicogildemuro/my-portfolio',
         demo: 'https://federico-gil-de-muro-portfolio.vercel.app/',
-        image: 'portfolio.jpg'
+        image: 'my-portfolio.jpg'
     },
     {
         name: 'Simple Chat',
         description: 'Basic chat application where users can join a room and send messages in real-time.',
-        techIcons: ['javascript-plain colored', 'nodejs-plain-wordmark colored', 'express-original', 'socketio-original', 'handlebars-original', 'bootstrap-plain colored'],
+        techIcons: [
+            { name: 'JavaScript', class: 'devicon-javascript-plain' },
+            { name: 'Node', class: 'devicon-nodejs-plain-wordmark' },
+            { name: 'Express', class: 'devicon-express-original' },
+            { name: 'Socket.io', class: 'devicon-socketio-original' },
+            { name: 'Handlebars', class: 'devicon-handlebars-original' },
+            { name: 'Bootstrap', class: 'devicon-bootstrap-plain' },
+        ],
         github: 'https://github.com/federicogildemuro/simple-chat',
         demo: 'https://husky-sordid-donkey.glitch.me/',
         image: 'simple-chat.jpg'
@@ -18,7 +28,11 @@ const projects = [
     {
         name: 'Kyo Sushi',
         description: 'E-commerce website for a sushi delivery. Users can browse the menu, add items to the cart, and place an order.',
-        techIcons: ['react-plain colored', 'firebase-plain colored', 'bootstrap-plain colored'],
+        techIcons: [
+            { name: 'React', class: 'devicon-react-original' },
+            { name: 'Firebase', class: 'devicon-firebase-plain' },
+            { name: 'Bootstrap', class: 'devicon-bootstrap-plain' },
+        ],
         github: 'https://github.com/federicogildemuro/kyo-sushi',
         demo: 'https://kyo-sushi.netlify.app/',
         image: 'kyo-sushi.jpg'
@@ -26,28 +40,48 @@ const projects = [
     {
         name: 'E-commerce API-REST',
         description: 'RESTful API for e-commerce, allowing users to browse products, manage their cart, and place orders.',
-        techIcons: ['javascript-plain colored', 'nodejs-plain-wordmark colored', 'express-original', 'socketio-original', 'handlebars-original', 'mongodb-plain colored'],
+        techIcons: [
+            { name: 'JavaScript', class: 'devicon-javascript-plain' },
+            { name: 'Node', class: 'devicon-nodejs-plain-wordmark' },
+            { name: 'Express', class: 'devicon-express-original' },
+            { name: 'Socket.io', class: 'devicon-socketio-original' },
+            { name: 'Handlebars', class: 'devicon-handlebars-original' },
+            { name: 'MongoDB', class: 'devicon-mongodb-plain' },
+        ],
         github: 'https://github.com/federicogildemuro/backend-coderhouse',
+        demo: 'https://e-commerce-api-rest.onrender.com/api/docs',
         image: 'ecommerce-api.jpg'
     },
     {
         name: 'Funko Shop',
         description: 'E-commerce website for a Funko Pop! store. Users can browse the catalog, add items to the cart, and place an order.',
-        techIcons: ['typescript-plain colored', 'angularjs-plain colored', 'firebase-plain colored'],
+        techIcons: [
+            { name: 'TypeScript', class: 'devicon-typescript-plain' },
+            { name: 'Angular', class: 'devicon-angularjs-plain' },
+            { name: 'Firebase', class: 'devicon-firebase-plain' },
+        ],
         github: 'https://github.com/maru33luc/FunkoStore',
+        demo: 'https://funko-store-seven.vercel.app/home',
         image: 'funko-shop.jpg'
     },
     {
         name: 'Docturno',
         description: 'Desktop application for managing appointments in a medical clinic. Patients can request appointments and doctors can manage their schedules.',
-        techIcons: ['java-plain colored'],
+        techIcons: [
+            { name: 'Java', class: 'devicon-java-plain' },
+        ],
         github: 'https://github.com/maru33luc/TP-Final-Lab3',
         image: 'docturno.jpg'
     },
     {
         name: 'Tom Misch Tribute',
         description: 'Web page paying tribute to the musician Tom Misch. Users can read about his life and discography, watch videos, and know his upcoming events.',
-        techIcons: ['html5-plain-wordmark colored', 'css3-plain-wordmark colored', 'sass-original colored', 'bootstrap-plain colored'],
+        techIcons: [
+            { name: 'HTML', class: 'devicon-html5-plain-wordmark' },
+            { name: 'CSS', class: 'devicon-css3-plain-wordmark' },
+            { name: 'Bootstrap', class: 'devicon-bootstrap-plain' },
+            { name: 'Sass', class: 'devicon-sass-original' },
+        ],
         github: 'https://github.com/federicogildemuro/desarrollo-web-coderhouse',
         demo: 'https://federicogildemuro.github.io/desarrollo-web-coderhouse/',
         image: 'tom-misch.jpg'
@@ -55,7 +89,9 @@ const projects = [
     {
         name: 'Prolijin',
         description: 'Program for managing clients, products, orders, suppliers, and cash flow, designed for efficient data handling and streamlined operations.',
-        techIcons: ['c-plain colored'],
+        techIcons: [
+            { name: 'C', class: 'devicon-c-plain' },
+        ],
         github: 'https://github.com/federicogildemuro/laboratorio2-tup',
         image: 'prolijin.jpg'
     }
