ab2ff756a7428d2582a41bb74bd371146be9de4c
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-05-07 12:38:45 -0300

    feat(about): update cv file and tech icons

--- src/data/technologies.js
+++ src/data/technologies.js
@@ -1,42 +1,39 @@
 const frontendIcons = [
-    { name: 'HTML', class: 'devicon-html5-plain-wordmark' },
-    { name: 'CSS', class: 'devicon-css3-plain-wordmark' },
-    { name: 'Bootstrap', class: 'devicon-bootstrap-plain' },
-    { name: 'Sass', class: 'devicon-sass-original' },
-    { name: 'JavaScript', class: 'devicon-javascript-plain' },
-    { name: 'TypeScript', class: 'devicon-typescript-plain' },
-    { name: 'Angular', class: 'devicon-angular-plain' },
-    { name: 'React', class: 'devicon-react-original' },
-    { name: 'Tailwind', class: 'devicon-tailwindcss-original' },
-    { name: 'Alpine', class: 'devicon-alpinejs-original' },
+    { name: "HTML", class: "devicon-html5-plain-wordmark" },
+    { name: "CSS", class: "devicon-css3-plain-wordmark" },
+    { name: "Sass", class: "devicon-sass-original" },
+    { name: "Bootstrap", class: "devicon-bootstrap-plain" },
+    { name: "JavaScript", class: "devicon-javascript-plain" },
+    { name: "TypeScript", class: "devicon-typescript-plain" },
+    { name: "Angular", class: "devicon-angular-plain" },
+    { name: "React", class: "devicon-react-original" },
+    { name: "Tailwind", class: "devicon-tailwindcss-original" },
+    { name: "Vue", class: "devicon-vuejs-plain" },
 ];
 
 const backendIcons = [
-    { name: 'C', class: 'devicon-c-plain' },
-    { name: 'Java', class: 'devicon-java-plain' },
-    { name: 'Node', class: 'devicon-nodejs-plain-wordmark' },
-    { name: 'Express', class: 'devicon-express-original' },
-    { name: 'Socket.io', class: 'devicon-socketio-original' },
-    { name: 'Handlebars', class: 'devicon-handlebars-original' },
-    { name: 'PHP', class: 'devicon-php-plain' },
-    { name: 'Laravel', class: 'devicon-laravel-original' },
+    { name: "C", class: "devicon-c-plain" },
+    { name: "Java", class: "devicon-java-plain" },
+    { name: "Node", class: "devicon-nodejs-plain-wordmark" },
+    { name: "Express", class: "devicon-express-original" },
+    { name: "PHP", class: "devicon-php-plain" },
+    { name: "Laravel", class: "devicon-laravel-original" },
 ];
 
 const databaseIcons = [
-    { name: 'MySQL', class: 'devicon-mysql-original' },
-    { name: 'Firebase', class: 'devicon-firebase-plain' },
-    { name: 'MongoDB', class: 'devicon-mongodb-plain' },
+    { name: "MySQL", class: "devicon-mysql-original" },
+    { name: "Firebase", class: "devicon-firebase-plain" },
+    { name: "MongoDB", class: "devicon-mongodb-plain" },
 ];
 
 const toolsIcons = [
-    { name: 'Git', class: 'devicon-git-plain' },
-    { name: 'GitHub', class: 'devicon-github-original' },
-    { name: 'Visual Studio Code', class: 'devicon-vscode-plain' },
-    { name: 'Postman', class: 'devicon-postman-plain' },
-    { name: 'Markdown', class: 'devicon-markdown-original' },
-    { name: 'Jira', class: 'devicon-jira-plain' },
-    { name: 'Trello', class: 'devicon-trello-plain' },
-    { name: 'Docker', class: 'devicon-docker-plain' },
+    { name: "Git", class: "devicon-git-plain" },
+    { name: "GitHub", class: "devicon-github-original" },
+    { name: "Postman", class: "devicon-postman-plain" },
+    { name: "Markdown", class: "devicon-markdown-original" },
+    { name: "Jira", class: "devicon-jira-plain" },
+    { name: "Trello", class: "devicon-trello-plain" },
+    { name: "Docker", class: "devicon-docker-plain" },
 ];
 
 export { frontendIcons, backendIcons, databaseIcons, toolsIcons };
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,43 +1,81 @@
-import { frontendIcons, backendIcons, databaseIcons, toolsIcons } from '../data/technologies';
+import {
+    frontendIcons,
+    backendIcons,
+    databaseIcons,
+    toolsIcons,
+} from "../data/technologies";
 
 function About() {
     return (
         <section className="flex flex-col justify-start items-center gap-10 p-10 lg:p-20 text-center text-color-amber">
-            <p>I'm a Full Stack Developer with a background as a former lawyer and professor, bringing a unique, multidisciplinary approach to problem-solving, leveraging skills from both legal and educational fields.</p>
-            <p>My journey has taught me to be detail-oriented, adaptable, and committed to continuous learning. I thrive on challenges and always strive to improve my skills.</p>
-            <p>Passionate about building innovative web applications, I enjoy exploring new technologies and crafting solutions that make a real impact.</p>
+            <p>
+                I'm a Full Stack Developer with a background as a former lawyer
+                and professor, bringing a unique, multidisciplinary approach to
+                problem-solving, leveraging skills from both legal and
+                educational fields.
+            </p>
+            <p>
+                My journey has taught me to be detail-oriented, adaptable, and
+                committed to continuous learning. I thrive on challenges and
+                always strive to improve my skills.
+            </p>
+            <p>
+                Passionate about building innovative web applications, I enjoy
+                exploring new technologies and crafting solutions that make a
+                real impact.
+            </p>
             <p>These are some of the technologies I work with...</p>
 
+            <h3 className="text-center">Frontend</h3>
             <div className="flex flex-wrap justify-center items-center gap-5">
-                <h3>Frontend:</h3>
                 {frontendIcons.map((icon, index) => (
-                    <i key={index} className={`${icon.class} text-3xl`} title={icon.name} aria-label={icon.name} />
+                    <i
+                        key={index}
+                        className={`${icon.class} text-3xl`}
+                        title={icon.name}
+                        aria-label={icon.name}
+                    />
                 ))}
             </div>
 
+            <h3 className="text-center">Backend</h3>
             <div className="flex flex-wrap justify-center items-center gap-5">
-                <h3>Backend:</h3>
                 {backendIcons.map((icon, index) => (
-                    <i key={index} className={`${icon.class} text-3xl`} title={icon.name} aria-label={icon.name} />
+                    <i
+                        key={index}
+                        className={`${icon.class} text-3xl`}
+                        title={icon.name}
+                        aria-label={icon.name}
+                    />
                 ))}
             </div>
 
+            <h3 className="text-center">Databases</h3>
             <div className="flex flex-wrap justify-center items-center gap-5">
-                <h3>Databases:</h3>
                 {databaseIcons.map((icon, index) => (
-                    <i key={index} className={`${icon.class} text-3xl`} title={icon.name} aria-label={icon.name} />
+                    <i
+                        key={index}
+                        className={`${icon.class} text-3xl`}
+                        title={icon.name}
+                        aria-label={icon.name}
+                    />
                 ))}
             </div>
 
+            <h3 className="text-center">Tools</h3>
             <div className="flex flex-wrap justify-center items-center gap-5">
-                <h3>Tools:</h3>
                 {toolsIcons.map((icon, index) => (
-                    <i key={index} className={`${icon.class} text-3xl`} title={icon.name} aria-label={icon.name} />
+                    <i
+                        key={index}
+                        className={`${icon.class} text-3xl`}
+                        title={icon.name}
+                        aria-label={icon.name}
+                    />
                 ))}
             </div>
 
             <p>
-                You can download my CV by clicking {''}
+                You can download my CV by clicking {""}
                 <a
                     href="cv-federico-gil-de-muro.pdf"
                     target="_blank"
